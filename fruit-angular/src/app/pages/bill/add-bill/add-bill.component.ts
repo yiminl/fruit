f41b37d78d49697f1a5b7af30723723029d1dfd5
Yimin Liu （刘益民） <yimin.liu@citrix.com>
2024-01-10 16:59:26 +0800

    add backend

--- fruit-angular/src/app/pages/bill/add-bill/add-bill.component.ts
+++ fruit-angular/src/app/pages/bill/add-bill/add-bill.component.ts
@@ -8,14 +8,80 @@ import { Component } from '@angular/core';
 export class AddBillComponent {
   isVisible = false;
   isConfirmLoading = false;
+  amount = '';
+  price = '';
 
   constructor() {}
+
+  ngOnInit() {
+    const addBillElement = document.getElementById('add-bill');
+    if (addBillElement) {
+      addBillElement.style.backgroundColor='red';  
+    }
+    addBillElement?.addEventListener('click',() =>{
+        const ammountElement = document.getElementById('Amount');
+
+        if(!ammountElement) {
+          console.log('amount element is null');
+        } else {
+          console.log('amount element is not null');
+        }
+
+        ammountElement?.addEventListener('mouseover',()=>{ 
+          if (ammountElement) {
+            ammountElement.style.backgroundColor='yellow'
+          }
+        })  
+        ammountElement?.addEventListener('mouseout',()=>{ 
+          if (ammountElement) {
+            ammountElement.style.backgroundColor='white'
+          }
+        })
+        
+        const priceElement = document.getElementById('Price');
+        priceElement?.addEventListener('mouseover',()=>{ 
+          if (priceElement) {
+            priceElement.style.backgroundColor='yellow'
+          }
+        })
+        priceElement?.addEventListener('mouseout',()=>{ 
+          if (priceElement) {
+            priceElement.style.backgroundColor='white'
+          }
+        })
+
+        let parent = document.getElementById('input');
+        let child = document.createElement('p');
+        child.id = 'p-id';
+        child.innerHTML = '<strong>created paragraf</strong>';
+        let textNode1 = document.createTextNode(' text node1 ');
+        let textNode2 = document.createTextNode(' text node2 ');
+        child.appendChild(textNode1);
+        child.appendChild(textNode2);
+        parent?.appendChild(child);
+    });
+    
+  }
 
   showModal(): void {
     this.isVisible = true;
   }
 
+  getAmount(event:Event, value: string) {
+    console.log((event as KeyboardEvent).ctrlKey);
+    console.log((event as KeyboardEvent).key);
+    console.log(value);
+    this.price = value
+
+    //const ammountElement = document.getElementById('Amount');
+    //this.price = +(ammountElement as HTMLInputElement).value;
+    
+    console.log(this.price);
+
+  }
+
   handleOk(): void {
+    console.log(this.price, this.amount);
     this.isConfirmLoading = true;
     setTimeout(() => {
       this.isVisible = false;
--- lym-server1/src/main/java/com/lym/app1/lymserver1/LymServer1Application.java
+++ lym-server1/src/main/java/com/lym/app1/lymserver1/LymServer1Application.java
@@ -8,10 +8,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LymServer1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(LymServer1Application.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 
 }
